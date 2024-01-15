9f2acc80f20396f8bc26d502f8d74d14810e854c
davidmikulic44 <david.mikulic44@gmail.com>
2024-01-15 16:12:07 +0100

    typography scss

--- src/components/Header/Header.vue
+++ src/components/Header/Header.vue
@@ -4,14 +4,19 @@
 
 <template>
     <header class="header-container">
-        <img src="@/assets/Vector Smart Objecta 1.svg" alt="">
-        
-        <h1 id="header-title">Kenaz</h1>
-        <ul>
-            <li>Media</li>
-            <li>Marketing</li>
-            <li>Contact</li>
-        </ul>
-        <img src="@/assets/search 1.svg">
+        <div class="logo-section">
+            <img src="@/assets/Vector Smart Objecta 1.svg" alt="">
+            <h1 id="header-title">Kenaz</h1>
+        </div>
+
+
+        <div class="navigation-section">
+            <nav id="mainNavigation">
+                <h3 class="main-nav-item">Media</h3>
+                <h3 class="main-nav-item">Marketing</h3>
+                <h3 class="main-nav-item">Contact</h3>
+            </nav>
+            <img src="@/assets/search 1.svg" alt="Search" id="searchIcon">
+        </div>
     </header>
 </template>
