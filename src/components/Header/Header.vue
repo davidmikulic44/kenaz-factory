b2e2a6f5d8266e794500845f6f172c982ffdc13d
davidmikulic44 <david.mikulic44@gmail.com>
2024-01-15 17:07:37 +0100

    header

--- src/components/Header/Header.vue
+++ src/components/Header/Header.vue
@@ -1,12 +1,15 @@
 <script setup>
-    import './styles/header.scss'
+    import './styles/_header.scss'
 </script>
 
 <template>
-    <header class="header-container">
+    <div class="container">
+        <header class="header-container">
         <div class="logo-section">
-            <img src="@/assets/Vector Smart Objecta 1.svg" alt="">
-            <h1 id="header-title">Kenaz</h1>
+            <div class="logo-container">
+                <img id="logo" src="@/assets/Vector Smart Objecta 1.svg" alt="">
+            </div>
+            <h1 id="headerTitle">Kenaz</h1>
         </div>
 
 
@@ -19,4 +22,5 @@
             <img src="@/assets/search 1.svg" alt="Search" id="searchIcon">
         </div>
     </header>
+    </div>
 </template>
