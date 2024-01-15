780fcd973ae213d5eb2d25cc43fabbfcd9c4c84a
davidmikulic44 <david.mikulic44@gmail.com>
2024-01-15 15:34:44 +0100

    deleted template files

---
+++ src/components/Header/Header.vue
@@ -0,0 +1,17 @@
+<script setup>
+    import './styles/header.scss'
+</script>
+
+<template>
+    <header class="header-container">
+        <img src="@/assets/Vector Smart Objecta 1.svg" alt="">
+        
+        <h1 id="header-title">Kenaz</h1>
+        <ul>
+            <li>Media</li>
+            <li>Marketing</li>
+            <li>Contact</li>
+        </ul>
+        <img src="@/assets/search 1.svg">
+    </header>
+</template>
