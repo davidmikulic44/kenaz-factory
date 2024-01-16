55a24e404848a62834c43082b24e3ee7955ad838
davidmikulic44 <david.mikulic44@gmail.com>
2024-01-16 09:55:56 +0100

    second header active buttons

--- src/components/Header/Categories.vue
+++ src/components/Header/Categories.vue
@@ -1,18 +1,67 @@
 <script setup>
-    import './styles/_categories.scss'
+  import { ref } from 'vue';
+  import './styles/_categories.scss';
+
+  const activeCategory = ref('news');
+
+  const handleClick = (category) => {
+    activeCategory.value = category;
+  };
 </script>
 
 <template>
-    <div class="header-container-big secondary">
-        <header class="header-container">
-            <ul class="main-navigation">
-                <li class="category-nav-item" id="category1">news</li>
-                <li class="category-nav-item" id="category2">business</li>
-                <li class="category-nav-item" id="category3">sport</li>
-                <li class="category-nav-item" id="category4">life</li>
-                <li class="category-nav-item" id="category5">tech</li>
-                <li class="category-nav-item" id="category6">travel</li>
-            </ul>
-        </header>
-    </div>
+  <div class="header-container-big secondary">
+    <header class="header-container">
+      <ul class="main-navigation">
+        <li
+          class="category-nav-item"
+          :class="{ 'active': activeCategory === 'news' }"
+          @click="handleClick('news')"
+          id="category1"
+        >
+          news
+        </li>
+        <li
+          class="category-nav-item"
+          :class="{ 'active': activeCategory === 'business' }"
+          @click="handleClick('business')"
+          id="category2"
+        >
+          business
+        </li>
+        <li
+          class="category-nav-item"
+          :class="{ 'active': activeCategory === 'sport' }"
+          @click="handleClick('sport')"
+          id="category3"
+        >
+          sport
+        </li>
+        <li
+          class="category-nav-item"
+          :class="{ 'active': activeCategory === 'life' }"
+          @click="handleClick('life')"
+          id="category4"
+        >
+          life
+        </li>
+        <li
+          class="category-nav-item"
+          :class="{ 'active': activeCategory === 'tech' }"
+          @click="handleClick('tech')"
+          id="category5"
+        >
+          tech
+        </li>
+        <li
+          class="category-nav-item"
+          :class="{ 'active': activeCategory === 'travel' }"
+          @click="handleClick('travel')"
+          id="category6"
+        >
+          travel
+        </li>
+      </ul>
+    </header>
+  </div>
 </template>
