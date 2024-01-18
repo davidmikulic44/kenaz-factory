f1c1701923eafa552a28f0a0306c3ad38024fde3
davidmikulic44 <david.mikulic44@gmail.com>
2024-01-18 10:35:42 +0100

    moved categories to a list

--- src/components/Header/Categories.vue
+++ src/components/Header/Categories.vue
@@ -2,6 +2,10 @@
   import { ref } from 'vue';
   import './styles/_categories.scss';
 
+  const categoryList = ['news', 'business', 
+                        'sport', 'life',
+                        'tech', 'travel',]
+
   const activeCategory = ref('category1');
 
   const handleClick = (category) => {
@@ -14,52 +18,14 @@
     <header class="header-container">
       <ul class="main-navigation">
         <li
+          v-for="(category, index) in categoryList"
+          :key="index"
           class="category-nav-item"
-          :class="{ 'active': activeCategory === 'category1' }"
-          @click="handleClick('category1')"
-          id="category1"
+          :class="{ 'active': activeCategory === 'category' + (index + 1) }"
+          @click="handleClick('category' + (index + 1))"
+          :id="'category' + (index + 1)"
         >
-          news
-        </li>
-        <li
-          class="category-nav-item"
-          :class="{ 'active': activeCategory === 'category2' }"
-          @click="handleClick('category2')"
-          id="category2"
-        >
-          business
-        </li>
-        <li
-          class="category-nav-item"
-          :class="{ 'active': activeCategory === 'category3' }"
-          @click="handleClick('category3')"
-          id="category3"
-        >
-          sport
-        </li>
-        <li
-          class="category-nav-item"
-          :class="{ 'active': activeCategory === 'category4' }"
-          @click="handleClick('category4')"
-          id="category4"
-        >
-          life
-        </li>
-        <li
-          class="category-nav-item"
-          :class="{ 'active': activeCategory === 'category5' }"
-          @click="handleClick('category5')"
-          id="category5"
-        >
-          tech
-        </li>
-        <li
-          class="category-nav-item"
-          :class="{ 'active': activeCategory === 'category6' }"
-          @click="handleClick('category6')"
-          id="category6"
-        >
-          travel
+          {{ category }}
         </li>
       </ul>
     </header>
