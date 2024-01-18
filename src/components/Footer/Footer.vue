3548f5fc992574e11fcb414e83bda3268c16cec7
davidmikulic44 <david.mikulic44@gmail.com>
2024-01-18 11:12:14 +0100

    changed names to be more consistent

--- src/components/Footer/Footer.vue
+++ src/components/Footer/Footer.vue
@@ -1,31 +1,31 @@
 <script setup>
     import './styles/_footer.scss'
-    import FooterSocial from './FooterSocial.vue';
-    import FooterNewsletter from './FooterNewsletter.vue';
-    import FooterTagsWidget from './FooterTagsWidget.vue';
-    import FooterSiteInfo from './FooterSiteInfo.vue';
-    import FooterFeaturedPosts from './FooterFeaturedPosts.vue';
-    import FooterRandomPosts from './FooterRandomPosts.vue';
-    import FooterTwitterFeed from './FooterTwitterFeed.vue';
+    import SocialLinks from './SocialLinks.vue';
+    import Newsletter from './Newsletter.vue';
+    import TagsWidget from './TagsWidget.vue';
+    import SiteInfo from './SiteInfo.vue';
+    import FeaturedPosts from './FeaturedPosts.vue';
+    import RandomPosts from './RandomPosts.vue';
+    import TwitterFeed from './TwitterFeed.vue';
 </script>
 
 <template>
     <footer class="footer-container-big">
         <div class="footer-primary">
             <div class="footer-row1">
-                <FooterSocial></FooterSocial>
-                <FooterNewsletter></FooterNewsletter>
-                <FooterTagsWidget></FooterTagsWidget>
+                <SocialLinks></SocialLinks>
+                <Newsletter></Newsletter>
+                <TagsWidget></TagsWidget>
             </div>
             <div class="footer-row2">
-                <FooterFeaturedPosts></FooterFeaturedPosts>
-                <FooterRandomPosts></FooterRandomPosts>
-                <FooterTwitterFeed></FooterTwitterFeed>
+                <FeaturedPosts></FeaturedPosts>
+                <RandomPosts></RandomPosts>
+                <TwitterFeed></TwitterFeed>
             </div>
         </div>
         <img src="../../assets/BigLine.svg">
         <div class="footer-secondary">
-            <FooterSiteInfo></FooterSiteInfo>
+            <SiteInfo></SiteInfo>
         </div>
     </footer>
     
