8820f95ea826d63337169731fa4b8c2196fda3dc
davidmikulic44 <david.mikulic44@gmail.com>
2024-01-23 09:10:14 +0100

    rated posts

--- src/components/Feed/FeedRatedPosts.vue
+++ src/components/Feed/FeedRatedPosts.vue
@@ -1,24 +1,87 @@
 <script setup>
+    import { ref } from 'vue';
+    const categoryList = ['popular', 'top rated', 'comments']
+
+    const activeCategory = ref('popular');
+
+    const handleClick = (category) => {
+        activeCategory.value = category;
+        
+    }
 </script>
 
 <template>
     <div class="rated-posts-container">
         <div class="rated-posts-header">
-            <h6 class="rated-posts-header-title">Popular</h6>
-            <h6 class="rated-posts-header-title">Top Rated</h6>
-            <h6 class="rated-posts-header-title">Comments</h6>
+            <h6
+            v-for="(category, index) in categoryList"
+            :key="index"
+            :class="{'active-rated' : activeCategory === (category)}"
+            @click="handleClick(category)"
+            :id="(category)"
+            class="rated-posts-header-title">{{ category }}</h6>
         </div>
         <div class="posts-container">
-            <div class="rated-posts-single">
-                <div class="rated-posts-single-info">
-                    <div class="footer-post-header">
-                        <h6 class="footer-post-info">January 22, 2024</h6>
-                        <h6 class="footer-post-info">22</h6>
+            <div class="rated-post-single">
+                <div class="rated-post-single-info">
+                    <div class="rated-post-header">
+                        <h6 class="rated-post-info comments">
+                            <img class="rated-post-comments-icon" src="../../assets/comment ico.svg">
+                            5</h6>
+                        <h6 class="rated-post-info date">January 22, 2024</h6>
                     </div>
                     <h3 class="rated-post-title">Palestinians call off peace talks after clash</h3>
                 </div>
                 <img src="../../assets/manofsteel.jpeg" class="footer-post-image" alt="">
             </div>
+            <div class="rated-post-single">
+                <div class="rated-post-single-info">
+                    <div class="rated-post-header">
+                        <h6 class="rated-post-info comments">
+                            <img class="rated-post-comments-icon" src="../../assets/comment ico.svg">
+                            5</h6>
+                        <h6 class="rated-post-info date">January 22, 2024</h6>
+                    </div>
+                    <h3 class="rated-post-title">Palestinians call off peace talks after clash</h3>
+                </div>
+                <img src="../../assets/manofsteel.jpeg" class="footer-post-image" alt="">
+            </div>
+            <div class="rated-post-single">
+                <div class="rated-post-single-info">
+                    <div class="rated-post-header">
+                        <h6 class="rated-post-info comments">
+                            <img class="rated-post-comments-icon" src="../../assets/comment ico.svg">
+                            5</h6>
+                        <h6 class="rated-post-info date">January 22, 2024</h6>
+                    </div>
+                    <h3 class="rated-post-title">Palestinians call off peace talks after clash</h3>
+                </div>
+                <img src="../../assets/manofsteel.jpeg" class="footer-post-image" alt="">
+            </div>
+            <div class="rated-post-single">
+                <div class="rated-post-single-info">
+                    <div class="rated-post-header">
+                        <h6 class="rated-post-info comments">
+                            <img class="rated-post-comments-icon" src="../../assets/comment ico.svg">
+                            5</h6>
+                        <h6 class="rated-post-info date">January 22, 2024</h6>
+                    </div>
+                    <h3 class="rated-post-title">Palestinians call off peace talks after clash</h3>
+                </div>
+                <img src="../../assets/manofsteel.jpeg" class="footer-post-image" alt="">
+            </div>
+            <div class="rated-post-single">
+                <div class="rated-post-single-info">
+                    <div class="rated-post-header">
+                        <h6 class="rated-post-info comments">
+                            <img class="rated-post-comments-icon" src="../../assets/comment ico.svg">
+                            5</h6>
+                        <h6 class="rated-post-info date">January 22, 2024</h6>
+                    </div>
+                    <h3 class="rated-post-title">Palestinians call off peace talks after clash</h3>
+                </div>
+                <img src="../../assets/manofsteel.jpeg" class="footer-post-image" alt="">
+            </div>  
             
         </div>
     </div>
