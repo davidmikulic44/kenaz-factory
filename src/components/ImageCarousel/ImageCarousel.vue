594528901a23853310bd90077b7f295e73388faf
davidmikulic44 <david.mikulic44@gmail.com>
2024-02-05 17:26:30 +0100

    gradient on carousel

--- src/components/ImageCarousel/ImageCarousel.vue
+++ src/components/ImageCarousel/ImageCarousel.vue
@@ -28,6 +28,7 @@ const splideOptions = {
   pagination: false,
   arrows: true,
   cover: true,
+  arrowPath: "M8.98536 50.0001C9.58857 50.0001 10.1672 49.7507 10.5937 49.3051L32.2873 26.6774C33.1756 25.7512 33.1756 24.2494 32.2873 23.3231L10.5937 0.695205C10.1672 0.249846 9.58857 0.000534058 8.98536 0.000534058C8.38218 0.000534058 7.80358 0.249846 7.37709 0.695205L1.64085 6.67859C0.75248 7.60481 0.75248 9.10737 1.64085 10.0336L15.9896 25.0007L1.64085 39.967C0.75248 40.8932 0.75248 42.3958 1.64085 43.322L7.37709 49.3051C7.80358 49.7507 8.38218 50.0001 8.98536 50.0001Z",
 };
 
 const changeImageSize = () => {
@@ -100,12 +101,13 @@ onMounted(() => {
     >
     <Splide ref="main" :options="splideOptions">
         <SplideSlide v-for="image in images" :key="image.id">
-            <div class="img-container">
-              <img :src="image.url" class="image-carousel-image">
+            <div class="img-container" :style="{backgroundImage: image.url}">
+              <img :src="image.url" class="image-carousel-image carousel-img">
+            </div>
             <div class="image-carousel-search-icon" @click="changeImageSize">
               <img v-if="!imageZoomed" src="../../assets/SearchIcon.svg" />
             </div>
-          </div>
+          
         </SplideSlide>
       </Splide>
     
